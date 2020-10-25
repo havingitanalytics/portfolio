f1b0ffff7ca868201350d08f36d079d6d807dc7b
havingitanalytics <miguelnunesrfm@outlook.com>
2020-10-25 18:05:33 +0000

    JS Object-Oriented Menu

--- app/assets/scripts/App.js
+++ app/assets/scripts/App.js
@@ -1,5 +1,7 @@
 import '../styles/styles.css'
-alert("Hello, this is just a test")
+import MobileMenu from './modules/MobileMenu'
+
+let mobileMenu = new MobileMenu();
 
 if (module.hot) {
     module.hot.accept()
---
+++ app/assets/scripts/modules/MobileMenu.js
@@ -0,0 +1,21 @@
+class MobileMenu {
+  constructor() {
+    /*A.Select elements from the site*/ 
+    this.menuIcon = document.querySelector(".site-header__menu-icon")
+    this.menuContent = document.querySelector(".site-header__menu-content")
+    this.siteHeader = document.querySelector(".site-header")
+    this.events()
+    }
+    /*B.Event handling with arrow function that indicates the method to be used*/
+    events() {
+      this.menuIcon.addEventListener("click", () => this.toggleTheMenu()) 
+    }
+    /*C.Defining functionality*/
+    toggleTheMenu() {
+       this.menuContent.classList.toggle("site-header__menu-content--is-visible")
+       this.siteHeader.classList.toggle("site-header--is-expanded")
+    }
+  }
+
+
+export default MobileMenu  /*to be included in App.js file (import code)*/
